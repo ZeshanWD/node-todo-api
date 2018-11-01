2b72bd447ed67e0bd047422510abb308ae1892f7
zeshanwd <zeshan9choudhary@gmail.com>
2018-11-01 14:25:07 +0100

    added route GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/Todo');
 const { User } = require('./models/User');
@@ -23,6 +23,16 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find()
     .then(todos => res.send({ todos }), e => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) return res.status(404).send('Todo not found');
+      res.send({ todo });
+    }).catch(err => res.status(400).send(err));
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
