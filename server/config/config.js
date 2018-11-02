03c7ff5ec515e0f4a44da678caa63bc543c69f63
zeshanwd <zeshan9choudhary@gmail.com>
2018-11-02 15:59:06 +0100

    setup separate enviroments.

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+const env = process.env.NODE_ENV;
+if (env == 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://zeshan:zeshan2018@ds157112.mlab.com:57112/mongo-todo-api';
+} else if (env == 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://zeshan:zeshan2018@ds249503.mlab.com:49503/mongo-todo-api-test';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const MONGO_URI = 'mongodb://zeshan:zeshan2018@ds157112.mlab.com:57112/mongo-todo-api';
 
-mongoose.connect(MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
