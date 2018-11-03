c10c7dfba96139756e9db7bb8294b903eda9e372
zeshanwd <zeshan9choudhary@gmail.com>
2018-11-03 20:03:04 +0100

    Add post /users route and token method.

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,6 @@
 const env = process.env.NODE_ENV;
+console.log('env *******', env);
+console.log('port *******', process.env.PORT);
 if (env == 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://zeshan:zeshan2018@ds157112.mlab.com:57112/mongo-todo-api';
